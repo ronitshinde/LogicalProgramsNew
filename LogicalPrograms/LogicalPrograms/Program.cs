9211bbf85bf285253815c8c157903face899df86
ronitshinde <rony.shinde753@gmail.com>
2023-02-08 18:21:42 +0530

    [Ronit] Add : UC1-Fibonanci Series

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Fibonanci Series\n2.Perfect Number\n3.Prime Number");
+            Console.WriteLine("Enter the option");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FibonanciSeries series = new FibonanciSeries();
+                    series.DisplayFibSeries();
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
